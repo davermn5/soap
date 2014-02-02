6b4f2c07fe515ce416e62c4eba2fc80ea4912675
Dave Roman <davermn5@gmail.com>
2014-02-01 20:51:31 -0800

    davermn5 : Fixed indentation.

--- HTTP_AUTH/server/pear_soap_example.php
+++ HTTP_AUTH/server/pear_soap_example.php
@@ -1,112 +1,107 @@
-<?php 
- 
- /*
-  *  Pear SOAP Server (works!)
-  */
- require_once('../../../php/SOAP/Server.php'); 
- 
- class ServiceClass
- {
-  public $__dispatch_map = array();
-  
-  public function __construct(){
-   $this->__dispatch_map['add'] = array(
-                                    'in' => array(
-									          'a' => "int", 
-									          'b' => "int"
-									),
-									
-									'out' => array(
-									           'c' => "int"
-									)
-   );
-   
-   $this->__dispatch_map['sub'] = array(
-                                    'in' => array(
-									          'd' => "int", 
-									          'e' => "int"
-									),
-									
-									'out' => array(
-									           'f' => "int"
-									)
-   );
-   
-   $this->__dispatch_map['mul'] = array(
-                                    'in' => array(
-									          'g' => "int", 
-									          'h' => "int"
-									),
-									
-									'out' => array(
-									           'i' => "int"
-									)
-   );
-   
-   $this->__dispatch_map['addthree'] = array(
-                                    'in' => array(
-									          'j' => "int", 
-									          'k' => "int",
-									          'l' => "int"
-									),
-									
-									'out' => array(
-									           'm' => "int"
-									)
-   );	
-  } //end constructor..
-  
-  public function __dispatch($method){
-   if( isset($this->__dispatch_map[$method]) )
-   {
-   	return $this->__dispatch_map[$method];
-   }
-   else{
-    return null;	
-   }	
-  } //end __dispatch()..
-  
-  
-  function add($a, $b){
-   return $a + $b;	
-  }
-  
-  function sub($d, $e){
-   return $d - $e;	
-  }
-  
-  function mul($g, $h){
-   return $g * $h;	
-  }
-  
-  function addthree($j, $k, $l){
-   return $j + $k + $l;	
-  }
-  
-  	
- } //end ServiceClass class..
- 
- 
- $soap = new SOAP_Server; 
- $service = new ServiceClass();
- 
- $soap->addObjectMap($service, 'urn:ServiceClass');  
-  
- if( isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST' )
- {
-  $soap->service($HTTP_RAW_POST_DATA);	
- }
- else{
-  require_once('../../../php/SOAP/Disco.php');
-  $disco = new SOAP_DISCO_Server($soap, 'ServiceClass');	
-  
-  if( isset($_SERVER['QUERY_STRING']) && strpos($_SERVER['QUERY_STRING'], 'wsdl') === 0 )
-  {
-   header('Content-type: text/xml');
-   echo $disco->getWSDL();	
-  }
- }
- 
- 
- 
-?>
+<?php
+
+	/**
+	 *  Pear SOAP Server
+	 */
+	require_once('../../../php/SOAP/Server.php');
+
+	class ServiceClass
+	{
+		public $__dispatch_map = array();
+
+		public function __construct()
+		{
+			$this->__dispatch_map['add'] = array(
+				'in' => array(
+					'a' => "int",
+					'b' => "int"
+				),
+
+				'out' => array(
+					'c' => "int"
+				)
+			);
+
+			$this->__dispatch_map['sub'] = array(
+				'in' => array(
+					'd' => "int",
+					'e' => "int"
+				),
+
+				'out' => array(
+					'f' => "int"
+				)
+			);
+
+			$this->__dispatch_map['mul'] = array(
+				'in' => array(
+					'g' => "int",
+					'h' => "int"
+				),
+
+				'out' => array(
+					'i' => "int"
+				)
+			);
+
+			$this->__dispatch_map['addthree'] = array(
+				'in' => array(
+					'j' => "int",
+					'k' => "int",
+					'l' => "int"
+				),
+
+				'out' => array(
+					'm' => "int"
+				)
+			);
+		} //end constructor..
+
+		public function __dispatch($method)
+		{
+			if (isset($this->__dispatch_map[$method])) {
+				return $this->__dispatch_map[$method];
+			} else {
+				return null;
+			}
+		} //end __dispatch()..
+
+
+		function add($a, $b)
+		{
+			return $a + $b;
+		}
+
+		function sub($d, $e)
+		{
+			return $d - $e;
+		}
+
+		function mul($g, $h)
+		{
+			return $g * $h;
+		}
+
+		function addthree($j, $k, $l)
+		{
+			return $j + $k + $l;
+		}
+	} //end ServiceClass class..
+
+
+	$soap = new SOAP_Server;
+	$service = new ServiceClass();
+
+	$soap->addObjectMap($service, 'urn:ServiceClass');
+
+	if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+		$soap->service($HTTP_RAW_POST_DATA);
+	} else {
+		require_once('../../../php/SOAP/Disco.php');
+		$disco = new SOAP_DISCO_Server($soap, 'ServiceClass');
+
+		if (isset($_SERVER['QUERY_STRING']) && strpos($_SERVER['QUERY_STRING'], 'wsdl') === 0) {
+			header('Content-type: text/xml');
+			echo $disco->getWSDL();
+		}
+	}
